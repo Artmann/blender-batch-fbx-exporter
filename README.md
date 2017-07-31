e4e4d7558710b98c0a079f44a4a28004acafa6b3
Christoffer Artmann <christoffer@artmann.co>
2017-07-31 18:12:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Blender Batch FBX Exporter
+
+This add-on lets you export your selected objects into separate FBX files which will be named according to the objects names in the hierarchy.
+
+## Usage
+
+In object mode, select the objects you wish to export and open the toolbox (Default: Space) and run `Batch Export FBX`. This will export
+the selected object to separate files in the same directory as your .blend file.
---
+++ main.py
@@ -0,0 +1,34 @@
+bl_info = {
+    "name": "Batch FBX Exporter", 
+    "category": "Object"
+}
+
+import bpy
+
+class BatchExporter(bpy.types.Operator):
+    """Batch FBX Exporter"""
+    bl_idname = "object.batch_fbx_export"
+    bl_label = "Batch Export FBX"
+    bl_options = {'REGISTER', 'UNDO'}
+    def execute(self, context):
+        objects = bpy.context.selected_objects
+        
+        for obj in objects:
+            bpy.ops.object.select_all(action='DESELECT')
+            path = bpy.path.abspath("//" + obj.name + ".fbx")
+            obj.select = True
+            bpy.ops.export_scene.fbx(filepath=path, use_selection=True)        
+        
+        for obj in objects:
+            obj.select = True
+
+        return { 'FINISHED' }
+
+def register():
+    bpy.utils.register_class(BatchExporter)
+
+def unregister():
+    bpy.utils.unregister_class(BatchExporter)
+
+if __name__ == "__main__":
+    register()
