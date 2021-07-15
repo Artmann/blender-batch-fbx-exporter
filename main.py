7819d3aff4cc12768a4d7a3404be1d309c26391b
Christoffer Artmann <Artgaard@gmail.com>
2021-07-15 12:46:20 +0200

    fix: Register the addon as a menu item so it shows up in newer Blender versions.

--- main.py
+++ main.py
@@ -1,32 +1,37 @@
 bl_info = {
-    "name": "Batch FBX Exporter", 
+    "blender": (2, 91, 2),
     "category": "Object",
-    "blender": (2, 80, 0)
+    "name": "Batch FBX Exporter"
 }
 
 import bpy
 
 class BatchExporter(bpy.types.Operator):
-    """Batch FBX Exporter"""
+    """Batch Export Selected Objects as FBX"""
     bl_idname = "object.batch_fbx_export"
-    bl_label = "Batch Export FBX"
+    bl_label = "Batch Export Selected Objects as FBX"
     bl_options = {'REGISTER', 'UNDO'}
+
     def execute(self, context):
         objects = bpy.context.selected_objects
-        
+
         for obj in objects:
             bpy.ops.object.select_all(action='DESELECT')
             path = bpy.path.abspath("//" + obj.name + ".fbx")
             obj.select_set(True)
-            bpy.ops.export_scene.fbx(filepath=path, use_selection=True)        
-        
+            bpy.ops.export_scene.fbx(filepath=path, use_selection=True)
+
         for obj in objects:
             obj.select_set(True)
 
         return { 'FINISHED' }
 
+def menu_func(self, context):
+    self.layout.operator(BatchExporter.bl_idname)
+
 def register():
     bpy.utils.register_class(BatchExporter)
+    bpy.types.VIEW3D_MT_object.append(menu_func)
 
 def unregister():
     bpy.utils.unregister_class(BatchExporter)
